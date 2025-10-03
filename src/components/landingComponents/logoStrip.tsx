1911277dd55b5e9840dc8aae344784ea558b1177
klyde fshq <klyde@fullstack.ph>
2025-10-03 14:51:41 +0800

    Icon

--- src/components/landingComponents/logoStrip.tsx
+++ src/components/landingComponents/logoStrip.tsx
@@ -31,11 +31,11 @@ export default function LogoStrip() {
         <div className="flex  space-between gap-20px py-52px px-81px border-1 border-black border-dashed overflow-x-hidden">
             <div className="flex py-10 animate-marquee whitespace-nowrap">
                 {logos.map((logo, i) => (
-                    <Image key={i} src={logo.src} alt={logo.alt} className="px-5" />
+                    <Image key={i} src={logo.src} alt={logo.alt} className="px-5 w-[100%]" />
                 ))}
                 {/* Repeat logos for seamless scrolling */}
                 {logos.map((logo, i) => (
-                    <Image key={i} src={logo.src} alt={logo.alt} className="px-5" />
+                    <Image key={i} src={logo.src} alt={logo.alt} className="px-5 w-[100%]" />
                 ))}
             </div>
         </div>
